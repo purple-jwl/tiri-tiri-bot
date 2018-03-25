2659ab7fbc423b3c1c919ed378b3e13079841fbd
purple-jwl <purple.jwl@gmail.com>
2018-03-26 00:59:25 +0900

    ランダムでカレーとトッピングを選ぶやつを実装

---
+++ scripts/index.js
@@ -0,0 +1,50 @@
+'use strict';
+
+const getCurry = () => {
+  const menu = [
+    'チャナ豆',
+    'レンズ豆',
+    'トマト',
+    'ポーク',
+    'チキン',
+    'ピーンズ（チャナ豆＋レンズ豆）',
+    'チーズ',
+    'シュリンプ',
+    'ミックス（トマト＋チーズ＋ホウレン草）',
+  ];
+
+  return menu[Math.floor(Math.random() * menu.length)];
+};
+
+const getExtras = (n) => {
+  const menu = [
+    'なま玉子',
+    'ゆで玉子',
+    'トマト',
+    'チャナ豆',
+    'レンズ豆',
+    'ホウレン草',
+    'レタス',
+    'チーズ',
+    'エビ',
+  ];
+
+  const extras = [];
+
+  while (n--) {
+    extras.push(menu[Math.floor(Math.random() * menu.length)]);
+  }
+
+  return extras;
+};
+
+module.exports = robot => {
+  robot.hear(/カレー/, response => {
+    response.send(':curry:');
+  });
+
+  robot.hear(/ルーレット/, response => {
+    const message = getCurry() + 'カレー ＋ ' + getExtras(1) + 'トッピング';
+    response.send(message);
+  })
+};
